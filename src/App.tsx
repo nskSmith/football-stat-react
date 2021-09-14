7e0b44866e7169c1ef10143d5728a44521e02623
Sergey Kovalev <nsk.kovalev@gmail.com>
2021-09-14 18:35:22 +0700

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This app uses football statistics from
+        </p>
+        <a
+          className="App-link"
+          href="https://www.sportmonks.com/football-api/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SPORTMONKS.COM
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
